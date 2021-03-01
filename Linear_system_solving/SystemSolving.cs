5e7b08f54d95b75ac60e3673bb54be91723e3a1b
DarkTowelie <valeriy.shestakov@inbox.ru>
2021-03-01 13:15:53 +0700

    v1.0

---
+++ Linear_system_solving/SystemSolving.cs
@@ -0,0 +1,95 @@
+﻿namespace Linear_system_solving
+{
+    class SystemSolving
+    {
+        private double[,] left;//Основная матрицы системы
+        private double[] right;//Вектор свободных компонент
+        //------------------------------------------------------------------------------------------------------
+        public SystemSolving(double[,] left, double[] right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        //------------------------------------------------------------------------------------------------------
+        public double[] gauss()
+        {
+            double[,] copyLeft = (double[,])left.Clone();
+            double[] copyRight = (double[])right.Clone();
+            int rows = copyLeft.GetLength(0);
+            int cols = copyLeft.GetLength(1);
+            int replaceInd;
+            double buf;
+
+            //Прямой пробег
+            for (int rInd = 0; rInd < rows; rInd++)
+            {
+                if (copyLeft[rInd, rInd] == 0)
+                {
+                    replaceInd = rInd;
+                    for (int j = rInd + 1; j < rows; j++)
+                    {
+                        if (copyLeft[j, rInd] != 0)
+                        {
+                            replaceInd = j;
+                            break;
+                        }
+                    }
+
+                    if (replaceInd != rInd)
+                    {
+                        for (int cInd = 0; cInd < cols; cInd++)
+                        {
+                            buf = copyLeft[rInd, cInd];
+                            copyLeft[rInd, cInd] = copyLeft[replaceInd, cInd];
+                            copyLeft[replaceInd, cInd] = buf;
+                        }
+
+                        buf = copyRight[rInd];
+                        copyRight[rInd] = copyRight[replaceInd];
+                        copyRight[replaceInd] = buf;
+                    }
+                }
+
+                if (copyLeft[rInd, rInd] != 0)
+                {
+                    buf = copyLeft[rInd, rInd];
+                    for (int cIndex = 0; cIndex < cols; cIndex++)
+                    {
+                        copyLeft[rInd, cIndex] /= buf;
+                    }
+                    copyRight[rInd] /= buf;
+
+
+                    for (int j = rInd + 1; j < rows; j++)
+                    {
+                        buf = copyLeft[j, rInd];
+                        for (int cIndex = 0; cIndex < cols; cIndex++)
+                        {
+                            copyLeft[j, cIndex] -= copyLeft[rInd, cIndex] * buf;
+                        }
+                        copyRight[j] -= copyRight[rInd] * buf;
+                    }
+                }
+            }
+
+            //Обратный пробег
+            for (int rInd = rows - 1; rInd > 0; rInd--)
+            {
+                for (int j = rInd - 1; j >= 0; j--)
+                {
+                    if (copyLeft[j, rInd] != 0)
+                    {
+                        buf = copyLeft[j, rInd];
+
+                        for (int cInd = 0; cInd < cols; cInd++)
+                        {
+                            copyLeft[j, cInd] -= copyLeft[rInd, cInd] * buf;
+                        }
+                        copyRight[j] -= copyRight[rInd] * buf;
+                    }
+                }
+            }
+            return copyRight;
+        }
+    }
+}
---
+++ Linear_system_solving/Tests.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Linear_system_solving
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+            SystemSolving systemSolving = null;
+            //------------------------------------------------------------------------------------------------------
+            bool testSuccess = true;
+            double[] testRes = new double[6];
+            double[] testRight = new double[6];
+            double[,] testLeft = new double[6, 6];
+
+            //Тест 1 - классическая система-------------------------------------------------------------------------
+            string startupPath = System.IO.Directory.GetCurrentDirectory();
+            string testDataPath = startupPath + "\\Test\\TEST_gauss_system_solving1.dat";
+            string inputDataPath = startupPath + "\\Test\\RESULT_gauss_system_solving1.dat";
+            
+            readTestFile(ref testLeft, ref testRight, testDataPath);
+            readResultFile(ref testRes, inputDataPath);
+
+            systemSolving = new SystemSolving(testLeft, testRight);
+            double[] res = systemSolving.gauss();
+
+            testSuccess = true;
+            for (int i = 0; i < 6; i++)
+            {
+                if (Math.Abs(res[i] - testRes[i]) > Math.Abs(0.01))
+                {
+                    testSuccess= false;
+                    Console.Write("TEST1 FAIL \n");
+                    break;
+                } 
+            }
+
+            if (testSuccess == true)
+                Console.Write("TEST1 SUCCESS \n");
+
+            //Тест 2 - нулевой элемент на главной диагонали----------------------------------------------------------
+            testDataPath = startupPath + "\\Test\\TEST_gauss_system_solving2.dat";
+            inputDataPath = startupPath + "\\Test\\RESULT_gauss_system_solving2.dat";
+
+            readTestFile(ref testLeft, ref testRight, testDataPath);
+            readResultFile(ref testRes, inputDataPath);
+
+            systemSolving = new SystemSolving(testLeft, testRight);
+            res = systemSolving.gauss();
+
+            testSuccess = true;
+            for (int i = 0; i < 6; i++)
+            {
+                if (Math.Abs(res[i] - testRes[i]) > Math.Abs(0.01))
+                {
+                    testSuccess = false;
+                    Console.Write("TEST2 FAIL \n");
+                    break;
+                }
+            }
+
+            if (testSuccess == true)
+                Console.Write("TEST2 SUCCESS \n");
+
+            //Тест 3 - случайный нулевой элемент--------------------------------------------------------------------
+            testDataPath = startupPath + "\\Test\\TEST_gauss_system_solving3.dat";
+            inputDataPath = startupPath + "\\Test\\RESULT_gauss_system_solving3.dat";
+
+            readTestFile(ref testLeft, ref testRight, testDataPath);
+            readResultFile(ref testRes, inputDataPath);
+
+            systemSolving = new SystemSolving(testLeft, testRight);
+            res = systemSolving.gauss();
+
+            testSuccess = true;
+            for (int i = 0; i < 6; i++)
+            {
+                if (Math.Abs(res[i] - testRes[i]) > Math.Abs(0.01))
+                {
+                    testSuccess = false;
+                    Console.Write("TEST3 FAIL \n");
+                    break;
+                }
+            }
+
+            if (testSuccess == true)
+                Console.Write("TEST3 SUCCESS \n");
+
+            //Тест 4 - почти нулевые стобцы-----------------------------------------------------------------------
+            testDataPath = startupPath + "\\Test\\TEST_gauss_system_solving4.dat";
+            inputDataPath = startupPath + "\\Test\\RESULT_gauss_system_solving4.dat";
+
+            readTestFile(ref testLeft, ref testRight, testDataPath);
+            readResultFile(ref testRes, inputDataPath);
+
+            systemSolving = new SystemSolving(testLeft, testRight);
+            res = systemSolving.gauss();
+            testSuccess = true;
+            for (int i = 0; i < 6; i++)
+            {
+                if (Math.Abs(res[i] - testRes[i]) > 0.01)
+                {
+                    testSuccess = false;
+                    Console.Write("TEST4 FAIL \n");
+                    break;
+                }
+            }
+
+            if (testSuccess == true)
+                Console.Write("TEST4 SUCCESS \n");
+
+            //Тест 5 - нулевая нижняя часть-------------------------------------------------------------------------
+
+            testDataPath = startupPath + "\\Test\\TEST_gauss_system_solving5.dat";
+            inputDataPath = startupPath + "\\Test\\RESULT_gauss_system_solving5.dat";
+
+            readTestFile(ref testLeft, ref testRight, testDataPath);
+            readResultFile(ref testRes, inputDataPath);
+
+            systemSolving = new SystemSolving(testLeft, testRight);
+            res = systemSolving.gauss();
+            testSuccess = true;
+            for (int i = 0; i < 6; i++)
+            {
+                if (Math.Abs(res[i] - testRes[i]) > 0.01)
+                {
+                    testSuccess = false;
+                    Console.Write("TEST5 FAIL \n");
+                    break;
+                }
+            }
+
+            if (testSuccess == true)
+                Console.Write("TEST5 SUCCESS \n");
+            //------------------------------------------------------------------------------------------------------
+            Console.ReadKey(); 
+        }
+        public static void readTestFile(ref double[,] A, ref double[] b, string path)
+        {
+            string[] lines = File.ReadAllLines(path, Encoding.UTF8);
+
+            int i = 0;
+            foreach (string line in lines)
+            {
+                string sep = "\t";
+                string[] words = line.Split(sep.ToCharArray());
+
+                for (int j = 0; j < A.GetLength(0); j++)
+                {
+                    try
+                    {
+                        A[i, j] = Convert.ToDouble(words[j]);
+                    }
+                    catch
+                    {
+                        words[j] = words[j].Replace('.', ',');
+                        A[i, j] = Convert.ToDouble(words[j]);
+                    }
+                    
+                }
+                try
+                {
+                    b[i] = Convert.ToDouble(words[b.GetLength(0)]);
+                }
+                catch
+                {
+                    words[b.GetLength(0)] = words[b.GetLength(0)].Replace('.', ',');
+                    b[i] = Convert.ToDouble(words[b.GetLength(0)]);
+                }
+                
+                i++;
+            }
+        }
+        public static void readResultFile(ref double[] testRes, string path)
+        {
+            string[] lines = File.ReadAllLines(path, Encoding.UTF8);
+
+            int i = 0;
+            foreach (string line in lines)
+            {
+                string sep = "\t";
+                string[] words = line.Split(sep.ToCharArray());
+
+                try
+                {
+                    testRes[i] = Convert.ToDouble(words[0]);
+                }
+                catch
+                {
+                    words[0] = words[0].Replace('.', ',');
+                    testRes[i] = Convert.ToDouble(words[0]);
+                }
+                i++;
+            }
+        }
+    }
+}
